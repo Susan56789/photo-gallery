010973b388c6c1d3b7079424f1bd9f3054f41116
Susan Wairimu <mwihaki2susan@gmail.com>
2021-10-13 19:43:52 +0300

    added motion effect

--- src/components/imageGrid.js
+++ src/components/imageGrid.js
@@ -11,9 +11,14 @@ const Grid = () =>{
         <div className="img-grid">
 {docs && docs.map(doc =>(
     <motion.div className="img-wrap" key={doc.id} 
+   layout
     whileHover={{opacity:1}}
     >
-<img src={doc.url} alt="uploaded pic" />
+<motion.img src={doc.url} alt="uploaded pic" 
+initial={{opacity: 0}}
+animate ={{opacity: 1}}
+transition={{delay: 1}}
+/>
     </motion.div>
 ))}
         </div>
